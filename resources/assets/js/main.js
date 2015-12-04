749398399a10e6b7f908ca6c59c5c51070bd7f7d
Sidrit Trandafili <forge405@gmail.com>
2015-12-04 14:51:45 +0100

    changes sid - ibi Fri Dec  4 14:51:44 CET 2015

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -4,33 +4,42 @@ Carousel homepage
 -------------------------
 */
 $(document).ready(function() {
-	
-	$('.main-carousel').slick({
-	  infinite: true,
-	  slidesToShow: 1,
-	  autoplay: true,
-	  arrows: false	  
-	});
-	
+    
+    $('.main-carousel').slick({
+      infinite: true,
+      slidesToShow: 1,
+      autoplay: true,
+      arrows: false   
+    });
+    
 });
 
-$(function() {
-    var offset = $(".sidebar").offset();
-    var footerOffset = $("footer").offset();
-    var mbPos = footerOffset.top-30; /*30px extra space*/
-    var topPadding = 15;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > offset.top-20 ) {
-            if($(window).scrollTop() < mbPos) {
-                $(".sidebar").stop().animate({
-                   marginTop: $(window).scrollTop() - offset.top + topPadding
-                });
+animateSidebar();
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  animateSidebar();
+})
+
+function animateSidebar(){
+    $(function() {
+        var offset = $(".sidebar").offset();
+        var footerOffset = $("footer").offset();
+        var mbPos = footerOffset.top-30; /*30px extra space*/
+        var topPadding = 15;
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > offset.top-20 ) {
+                if($(window).scrollTop() < mbPos - $(".sidebar").height()) {
+                    $(".sidebar").stop().animate({
+                       marginTop: $(window).scrollTop() - offset.top + topPadding
+                    });
+                }
             }
-        }
-        else {
-            $(".sidebar").stop().animate({
-                marginTop: 0
-            });
-        };
+            else {
+                $(".sidebar").stop().animate({
+                    marginTop: 0
+                });
+            };
+        });
     });
-});
+}
+//# sourceMappingURL=main.js.map
