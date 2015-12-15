fb106b349f015356c6c8cf9ddac1c7514bff7a51
Sidrit Trandafili <forge405@gmail.com>
2015-12-15 11:18:50 +0100

    asd
    asd
    
    Signed-off-by: Sidrit Trandafili <forge405@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
         $out = '';
         $result = array();
         $result = shell_exec("/usr/bin/git -c /home/www/sidrit.com/ibi pull 2>&1");
-        return $result;
+        return '<pre>' .$result.'</pre>';
     }
 
 
